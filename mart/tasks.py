7c33aff0846ba41343ad2608765f5898d573b280
pyk <x24870@gmail.com>
2021-01-02 12:36:56 +0800

    find top selling products

--- mart/tasks.py
+++ mart/tasks.py
@@ -1,4 +1,4 @@
-import os, csv
+import os, csv, heapq
 from collections import defaultdict
 
 from django.conf import settings
@@ -33,8 +33,8 @@ def audit_sales():
         shop_sales[shop_id][SALES_QTY] += int(order.quantity)
         shop_sales[shop_id][ORDER_COUNT] += 1
 
+    file_name = os.path.join(settings.BASE_DIR, 'audit_sales.csv')
     try:
-        file_name = os.path.join(settings.BASE_DIR, 'audit_sales.csv')
         with open(file_name, 'w', newline='') as csvfile:
             writer = csv.writer(csvfile)
 
@@ -47,8 +47,50 @@ def audit_sales():
                 ])
                 
     except Exception as e:
-        print("Unable to generate 'audit_sales.csv'")
         print(e)
+        print(f'Unable to generate {file_name}')
+
+    find_most_popular()
 
 def find_most_popular():
-    print('******find_most_popular*******')
+    '''
+    Use heap sort to find top selling products.
+    If the amount of product is 'n',
+    and we want to find top 'k' products.
+    The time complexity will be O(nlogk)
+    '''
+
+    def return_defual_value():
+        return 0
+
+    shop_sales = defaultdict(return_defual_value)
+    orders = Order.objects.all()
+    
+    for order in orders:
+        shop_sales[order.product.id] += order.quantity
+
+    heap = []
+    top_selling = 3
+    for p_id, qty in shop_sales.items():
+        if len(heap) < top_selling:
+            heapq.heappush(heap, (qty, p_id))
+        else:
+            heapq.heappushpop(heap, (qty, p_id))
+
+    file_name = os.path.join(
+        settings.BASE_DIR, 
+        f'top_{top_selling}_selling_products.csv'
+        )
+    try:
+        with open(file_name, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            # heapd is min heap, reverse it to ascending order
+            heap.reverse() 
+            for h in heap:
+                writer.writerow([h[1], h[0]])
+                
+    except Exception as e:
+        print(e)
+        print(f'Unable to generate {file_name}')
+
+    
